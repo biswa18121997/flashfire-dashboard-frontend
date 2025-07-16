a02ee3410d8d5fa5bf12127141890536bc7bbf93
support1122 <support@flashfirejobs.com>
2025-07-17 01:15:44 +0530

    Update Register.tsx

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -16,6 +16,7 @@ export default function Register(){
     async function handleRegister() {
   try {
     const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+    console.log("API_BASE_URL:", API_BASE_URL);
     console.log(name, mail, password);
 
     const res = await fetch(`${API_BASE_URL}/register`, {
